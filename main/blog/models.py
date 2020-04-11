ac7e71ea47b2310ba0ee2adc9ba94932245f42f5
malijani <alijanialijanvandmohammad@gmail.com>
2020-04-11 16:25:43 +0430

    Using ckeditor RichTextField for content field

--- main/blog/models.py
+++ main/blog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from ckeditor.fields import RichTextField
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Post(models.Model):
+    """ The Post models for blog posts, One To Many relationship """
+    # if user got deleted, delete all related posts to user
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    content = RichTextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.title
