66a082a5b9ad64b66a8777417f8c4e76f4e1c9b8
malijani <alijanialijanvandmohammad@gmail.com>
2020-04-11 12:24:05 +0430

    Added some dummy post content for frontend configuration

--- main/blog/views.py
+++ main/blog/views.py
@@ -1,11 +1,25 @@
 from django.shortcuts import render
 
+# just a dummy post data to design frontend
+posts = [
+    {
+        'author': 'محمد علیجانی',
+        'title': 'اولین پست وبلاگ',
+        'content': 'محتوای اولین پست وبلاگ',
+        'date_posted': '۲۳ فروردین ۱۳۹۹',
+    },{
+        'author': 'یه بنده خدا',
+        'title': 'دومین پست وبلاگ',
+        'content': 'محتوای دومین پست وبلاگ',
+        'date_posted': '۲۵ فروردین ۱۳۹۹',
+    }
+]
 
 def home(request):
     # Frontend configuration
     template_name = 'blog/home.html'
     context = {
-
+        'posts': posts
     }
     return render(request, template_name, context)
 
