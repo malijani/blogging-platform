eae2e0075175909fbd9891483a2fb6e73c1a22d7
malijani <alijanialijanvandmohammad@gmail.com>
2020-04-11 23:17:40 +0430

    Added user registration view

--- main/users/views.py
+++ main/users/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import UserRegisterForm
 
-# Create your views here.
+def register(request):
+    # if data is submited
+    if request.method == "POST":
+        form = UserRegisterForm(request.POST)
+        # if form is valid when it is submited
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get("username")
+            messages.success(request, f'حساب کاربری {username} ایجاد شد!')
+            return redirect('blog:home')
+    else:
+        # create a user creation form
+        form = UserRegisterForm()
+    template_name = 'users/register.html'
+    context = {
+        'form': form,
+    }
+    return render(request, template_name, context)
