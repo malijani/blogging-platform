31350d92c8a29477b0e92fb3bdf1d943855b14fa
malijani <alijanialijanvandmohammad@gmail.com>
2020-04-12 21:45:45 +0430

    Created all CRUD generic class based views for blog app

--- main/blog/views.py
+++ main/blog/views.py
@@ -1,20 +1,67 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView)
 
 from .models import Post
 
 
 class PostListView(ListView):
     model = Post
-    #queryset = Post.objects.order_by('-date_posted')[:5]
+    queryset = Post.objects.order_by('-date_posted')[:10]
     context_object_name = 'posts'
-    ordering = ['-date_posted']
+    #ordering = ['-date_posted']
     template_name = 'blog/home.html'
+
 
 class PostDetailView(DetailView):
     model = Post
     template_name = 'blog/detail.html'
     #context_object_name = 'post'
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    template_name = 'blog/create.html'
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    template_name = 'blog/create.html'
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    template_name = 'blog/delete.html'
+    success_url = '/'
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+
 
 def about(request):
     # Frontend configuration
