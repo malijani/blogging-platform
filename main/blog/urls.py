f44639f78ccce6462974401cbba2e37f2c27b923
malijani <alijanialijanvandmohammad@gmail.com>
2020-04-12 17:48:12 +0430

    Added detail route

--- main/blog/urls.py
+++ main/blog/urls.py
@@ -5,6 +5,7 @@ from . import apps, views
 app_name = apps.BlogConfig.name
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.PostListView.as_view(), name='home'),
+    path('post/<int:pk>/', views.PostDetailView.as_view(), name='detail'),
     path('about/', views.about, name='about'),
 ]
