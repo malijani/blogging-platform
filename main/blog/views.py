881bb5a67b4028c26c7b2b88154b28aa8b2b0665
malijani <alijanialijanvandmohammad@gmail.com>
2020-04-12 17:48:00 +0430

    Created generic class views

--- main/blog/views.py
+++ main/blog/views.py
@@ -1,26 +1,20 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+
 from .models import Post
 
-# just a dummy post data to design frontend
-# posts = [
-#     {
-#         'author': 'محمد علیجانی',
-#         'title': 'اولین پست وبلاگ',
-#         'content': 'محتوای اولین پست وبلاگ',
-#         'date_posted': '۲۳ فروردین ۱۳۹۹',
-#     },
-# ]
 
-def home(request):
-    # QuerySet of Post objects
-    queryset = Post.objects.order_by('-date_posted')
-    # Frontend configuration
+class PostListView(ListView):
+    model = Post
+    #queryset = Post.objects.order_by('-date_posted')[:5]
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
     template_name = 'blog/home.html'
-    context = {
-        'posts': queryset,
-    }
-    return render(request, template_name, context)
 
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/detail.html'
+    #context_object_name = 'post'
 
 def about(request):
     # Frontend configuration
