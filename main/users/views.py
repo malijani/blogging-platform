d654614ba6616719112b7729011492a47496f5cb
malijani <alijanialijanvandmohammad@gmail.com>
2020-04-12 12:51:42 +0430

    Added profile view

--- main/users/views.py
+++ main/users/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+
 from .forms import UserRegisterForm
 
 def register(request):
@@ -10,13 +12,22 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get("username")
-            messages.success(request, f'حساب کاربری {username} ایجاد شد!')
-            return redirect('blog:home')
+            messages.success(request, f'حساب کاربری شما ساخته شد. الآن میتونید وارد حساب خودتون بشید!')
+            return redirect('users:login')
     else:
         # create a user creation form
         form = UserRegisterForm()
     template_name = 'users/register.html'
     context = {
         'form': form,
+    }
+    return render(request, template_name, context)
+
+
+@login_required
+def profile(request):
+    template_name = 'users/profile.html'
+    context = {
+
     }
     return render(request, template_name, context)
