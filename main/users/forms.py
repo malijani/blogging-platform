462e5455836cc04d48a5bb5413b215e36061772a
malijani <alijanialijanvandmohammad@gmail.com>
2020-04-12 15:39:05 +0430

    Created model forms for user and profile to change profile directly from website

--- main/users/forms.py
+++ main/users/forms.py
@@ -1,10 +1,12 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import Profile
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
     first_name = forms.CharField(max_length=100)
+
     class Meta:
         model = User
         fields = [
@@ -14,3 +16,21 @@ class UserRegisterForm(UserCreationForm):
             'password1',
             'password2',
         ]
+
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+    first_name = forms.CharField(max_length=100)
+    class Meta:
+        model = User
+        fields = [
+            'first_name',
+            'username',
+            'email',
+        ]
+
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['image']
