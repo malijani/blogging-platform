b904781f741ed4d2a5f189b084f7f6587fc66116
malijani <alijanialijanvandmohammad@gmail.com>
2020-04-12 15:37:42 +0430

    Updated profile view to change it from site frontend

--- main/users/views.py
+++ main/users/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 
 def register(request):
     # if data is submited
@@ -26,8 +26,32 @@ def register(request):
 
 @login_required
 def profile(request):
+    # make updates if form submited
+    if request.method == "POST":
+
+        u_form = UserUpdateForm(
+            request.POST,
+            instance=request.user
+         )
+        p_form = ProfileUpdateForm(
+            request.POST,
+            request.FILES,
+            instance=request.user.profile
+        )
+
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, f'حساب کاربری شما بروز رسانی شد!')
+            return redirect('users:profile')
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+
+    # configure frontend
     template_name = 'users/profile.html'
     context = {
-
+        'u_form': u_form,
+        'p_form': p_form,
     }
     return render(request, template_name, context)
