c1014c9707f618faa4780dc8560685cfa6ae8544
malijani <alijanialijanvandmohammad@gmail.com>
2020-04-12 21:46:06 +0430

    Updated CRUD routing for blog app

--- main/blog/urls.py
+++ main/blog/urls.py
@@ -6,6 +6,9 @@ app_name = apps.BlogConfig.name
 
 urlpatterns = [
     path('', views.PostListView.as_view(), name='home'),
+    path('post/create/', views.PostCreateView.as_view(), name='create'),
     path('post/<int:pk>/', views.PostDetailView.as_view(), name='detail'),
+    path('post/<int:pk>/update/', views.PostUpdateView.as_view(), name='update'),
+    path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='delete'),
     path('about/', views.about, name='about'),
 ]
