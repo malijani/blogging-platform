ec1cc21954ebb36b387eeb728eb86c726ed664ec
malijani <alijanialijanvandmohammad@gmail.com>
2020-04-12 21:47:34 +0430

    Updated models method

--- main/blog/models.py
+++ main/blog/models.py
@@ -1,7 +1,8 @@
 from django.db import models
-from ckeditor.fields import RichTextField
+from ckeditor import fields as ckeditor
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.shortcuts import reverse
 
 
 class Post(models.Model):
@@ -9,8 +10,11 @@ class Post(models.Model):
     # if user got deleted, delete all related posts to user
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
-    content = RichTextField()
+    content = ckeditor.RichTextField()
     date_posted = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('blog:detail', kwargs={'pk': self.pk})
