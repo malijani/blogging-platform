d7b6fea9f815e7fb4a4fe50acf35134d6d5ff3d1
malijani <alijanialijanvandmohammad@gmail.com>
2020-04-13 09:29:16 +0430

    Added rout for user specific blog posts

--- main/blog/urls.py
+++ main/blog/urls.py
@@ -6,6 +6,7 @@ app_name = apps.BlogConfig.name
 
 urlpatterns = [
     path('', views.PostListView.as_view(), name='home'),
+    path('user/<str:username>/', views.UserPostsListView.as_view(), name='profile'),
     path('post/create/', views.PostCreateView.as_view(), name='create'),
     path('post/<int:pk>/', views.PostDetailView.as_view(), name='detail'),
     path('post/<int:pk>/update/', views.PostUpdateView.as_view(), name='update'),
